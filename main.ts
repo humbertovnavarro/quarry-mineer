e0da2a7d575da6ecd9a4da4ac3bfc7d70fd402d3
humbertovnavaro <humberto.navarro.dev@gmail.com>
2022-10-23 16:10:25 -0700

    cleaned up code a little bit

--- main.ts
+++ main.ts
@@ -1,44 +1,39 @@
-import { draw } from "cc.image.nft";
-import * as strings from "cc.strings"
+const MAX_LOG_SIZE = 7;
 const INVENTORY_SLOTS = 16;
-const INVENTORY_SIZE = INVENTORY_SLOTS * 64;
 const MOTDS = [
 "try adding a chest behind your computer before mining to dump excess items", 
 "computers stop working outside of load distance, be careful!", 
 "don't get your computer stuck in someones claim, they will trespass!",
 "you can create a custom whitelist file by adding minecraft id's to whitelist.txt"
 ];
-const MOTD = MOTDS[Math.round(math.random() % MOTDS.length)]
-const WHITELIST = new Set<string>([
-
-]);
-
+let MOTD = MOTDS[Math.round(math.random() % MOTDS.length)];
+const WHITELIST = {}
 let whitelistFile: FileHandle;
 if(!fs.exists("whitelist.txt")) {
     const whitelistFile = fs.open("whitelist.txt", "w")[0];
-    const resp = http.get("https://pastebin.com/raw/WJfBSt3H")[2];
-    if(resp) {
+    io.write("downloading fresh whitelist...\m")
+    const resp = http.get("https://pastebin.com/raw/WJfBSt3H")[0] as HTTPResponse;
+    if(resp != undefined && resp.readAll) {
         whitelistFile.write(resp.readAll());
         whitelistFile.close();
+    } else {
+        throw new Error("could not fetch block whitelist!")
     }
+    io.write("done\n");
 }
-
 let line: string | undefined;
 whitelistFile = fs.open("whitelist.txt", "r")[0];
 do {
     line = whitelistFile.readLine();
     if(line) {
-        WHITELIST.add(line);
+        WHITELIST[line] = true;
     }
 } while(line != undefined);
 whitelistFile.close();
 
-const FUELS = new Set<string>([
-    "minecraft:lava",
-    "minecraft:coal",
-    "minecraft:charcoal",
-    "minecraft:coal_block",
-]);
+if(Object.keys(WHITELIST).length === 0) {
+    throw new Error("block whitelist is empty!")
+}
 
 
 enum Direction {
@@ -62,9 +57,12 @@ class MiningJob {
 }
 
 class MiningTurtle {
+    whitelist: {
+        [key: string]: string
+    }
     logs: string[] = [];
     job?: MiningJob;
-    gabageCollectionInterval = 32;
+    fixedUpdateInterval = 32;
     // left / right
     x: number = 0;
     // up / down
@@ -75,6 +73,23 @@ class MiningTurtle {
     steps: number = 0;
     facing: Direction = Direction.north;
     itemCount: number = 0;
+    constructor(whitelist: {
+        [key: string]: string
+    }) {
+        this.whitelist = whitelist;
+    };
+
+    addStep() {
+        this.steps++;
+        if(this.steps % this.fixedUpdateInterval == 0) {
+            this.onFixedUpdate();
+        }
+    }
+
+    onFixedUpdate() {
+        this.tick();
+    }
+
     turnLeft() {
         if(!turtle.turnLeft()) {
             throw new Error("could not turn left");
@@ -99,41 +114,41 @@ class MiningTurtle {
 
     face(target: Direction) {
         if(this.facing === target) return;
-
-        if(this.facing === Direction.west && target === Direction.north) {
-            this.turnRight();
-            return;
-        }
-        if(this.facing === Direction.north && target === Direction.west) {
-            this.turnLeft();
-            return;
-        }
-
         if(this.facing < target) {
             while(this.facing != target) {
                 this.turnLeft();
             }
-        }
-
-        if(this.facing > target) {
+        } else if(this.facing > target) {
             while(this.facing != target) {
                 this.turnRight();
             }
+        } else {
+            return;
         }
     }
 
     dumpInventory() {
-        this.face(Direction.south);
-        for(let i = 1; i <= INVENTORY_SIZE; i++) {
-            turtle.transferTo(1, 64);
+        for(let i = 1; i <= INVENTORY_SLOTS; i++) {
+            if(turtle.getItemDetail(i) !== undefined) {
+                turtle.select(i);
+                turtle.transferTo(1);
+            }
         }
     }
 
     log(text: string) {
-        if(this.logs.length > 5) {
+        if(this.logs.length > MAX_LOG_SIZE) {
             this.logs.shift();
         }
-        this.logs.push(text);
+        this.logs.push(`[${os.date("%M:%S")}] ${text}`);
+    }
+    
+    printDistance(): string {
+        if(this.steps > 1000) {
+            return `${string.format("%.2f"), this.steps / 1000}km`
+        } else {
+            return `${this.steps}m`
+        }
     }
 
     draw() {
@@ -143,6 +158,7 @@ class MiningTurtle {
             term.setTextColor(colors.white);
             io.write(`blocks mined: ${this.minedBlocks}/${this.job.totalBlocks}\n`);
             io.write(`${string.format("%.2f", this.minedBlocks / this.job.totalBlocks * 100)}% complete\n`);
+            io.write(`travelled ${this.printDistance()}\n`);
             io.write("=====================================\n")
             io.write(MOTD + "\n")
             io.write("=====================================\n")
@@ -150,27 +166,34 @@ class MiningTurtle {
             term.setTextColor(colors.black);
             this.logs.forEach(log => io.write(log.trim() + "\n"));
     }
-
-    garbageCollect() {
+    
+    tick() {
+        MOTD = MOTDS[Math.round(math.random() % MOTDS.length)];
+        let occupiedSlots = 0;
+        this.itemCount = 0;
         for(let i = 1; i <= INVENTORY_SLOTS; i++) {
-            this.itemCount = 0;
-            const item = turtle.getItemDetail() as {
-                name: string,
-                count: number
-            } | undefined;
-            turtle.select(i);
-            (item: {name: string, count: number} | undefined) => {
-                if(item === undefined) return;
-                if(item.name && WHITELIST.has(item.name)) {
-                    this.itemCount += item.count;
+            const item = turtle.getItemDetail(i);
+            if(item != undefined) {
+                if(this.whitelist[(item["name"])] != undefined) {
+                    this.itemCount += item["count"];
+                    occupiedSlots++;
                 } else {
+                    turtle.select(i);
                     turtle.drop();
                 }
-                if(this.itemCount + 32 > INVENTORY_SIZE) {
-                    this.log("going home to offload")
-                    this.dumpInventory();
-                }
-            };(item);
+            }
+        }
+        if(occupiedSlots == INVENTORY_SLOTS && this.itemCount > INVENTORY_SLOTS * 32) {
+            const x = this.x;
+            const y = this.y;
+            const z = this.z;
+            const facing = this.facing;
+            this.log("going home to offload");
+            this.goTo(x,y,z);
+            this.face(Direction.south)
+            this.dumpInventory();
+            this.goTo(x,y,z);
+            this.face(facing);
         }
     }
 
@@ -178,10 +201,10 @@ class MiningTurtle {
         if(!turtle.forward()) {
             throw new Error("could not move forward");
         }
+        this.addStep();
         this.draw();
-        this.steps++;
-        if(this.steps % this.gabageCollectionInterval == 0) {
-            this.garbageCollect();
+        if(this.steps % this.fixedUpdateInterval === 0) {
+            this.onFixedUpdate();
         }
         switch(this.facing) {
             case Direction.north:
@@ -200,7 +223,7 @@ class MiningTurtle {
     }
 
     up() {
-        this.steps++;
+        this.addStep();
         if(!turtle.up()) {
             throw new Error("could not move up");
         }
@@ -208,7 +231,7 @@ class MiningTurtle {
     }
 
     down() {
-        this.steps++;
+        this.addStep();
         if(!turtle.down()) {
             throw new Error("could not move down");
         }
@@ -230,41 +253,51 @@ class MiningTurtle {
         turtle.digUp();
     }
 
-    private mineRectangle(extent: number, depth: number, direction: Direction) {
-        for(let i = 0; i < depth; i++) {
-            for(let j = 0; j < extent - 1; j++) {
+    snakeRight(depth: number, extent: number) {
+        for(let j = 0; j < extent; j++) {
+            for(let k = 0; k < depth; k++) {
                 this.dig();
                 this.forward();
             }
-            if(i === depth -1) return;
-            switch(direction) {
-                case Direction.west:
-                    if(i % 2 === 0) {
-                        this.turnLeft();
-                        this.dig();
-                        this.forward();
-                        this.turnLeft();
-                    } else {
-                        this.turnRight();
-                        this.dig();
-                        this.forward();
-                        this.turnRight();
-                    }
-                    break;
-                default:
-                    if(i % 2 === 0) {
-                        this.turnRight();
-                        this.dig();
-                        this.forward();
-                        this.turnRight();
-                    } else {
-                        this.turnLeft();
-                        this.dig();
-                        this.forward();
-                        this.turnLeft();
-                    }
-                    break;
+            if(j % 2 === 0) {
+                this.turnRight();
+                this.dig();
+                this.forward();
+                this.turnRight();
+            } else {
+                this.turnLeft();
+                this.dig();
+                this.forward();
+                this.turnLeft();
             }
+        }
+        for(let k = 0; k < depth; k++) {
+            this.dig();
+            this.forward();
+        }
+    }
+
+    snakeLeft(depth: number, extent: number) {
+        for(let j = 0; j < extent; j++) {
+            for(let k = 0; k < depth; k++) {
+                this.dig();
+                this.forward();
+            }
+            if(j % 2 !== 0) {
+                this.turnRight();
+                this.dig();
+                this.forward();
+                this.turnRight();
+            } else {
+                this.turnLeft();
+                this.dig();
+                this.forward();
+                this.turnLeft();
+            }
+        }
+        for(let k = 0; k < depth; k++) {
+            this.dig();
+            this.forward();
         }
     }
 
@@ -278,72 +311,114 @@ class MiningTurtle {
         depth = math.abs(depth);
         height = math.abs(height);
         for(let i = 0; i < height; i++) {
-            const upperRightCorner = this.x === extent - 1 && this.z === depth - 1;
-            const bottomLeftCorner = this.x === 0 && this.z === 0;
-            if(bottomLeftCorner) {
+            if(this.x === 0) {
                 this.face(Direction.north);
-            }
-            if(upperRightCorner) {
-                this.face(Direction.south);
-            }
-            if(bottomLeftCorner || upperRightCorner) {
-                this.mineRectangle(extent, depth, Direction.east);
-            }
-            if(i === height - 1) {
-                return;
-            }
-            if(mineDown) {
-                this.digDown();
-                this.down();
+                this.snakeRight(depth - 1, extent - 1);
+            } else if(this.z > 0) {
+                this.turnLeft();
+                this.turnLeft();
+                this.snakeRight(depth - 1, extent - 1);
             } else {
-                this.digUp();
-                this.up();
+                this.turnRight();
+                this.turnRight();
+                this.snakeLeft(depth - 1, extent - 1);
+            }
+            if(i != height - 1) {
+                if(mineDown) {
+                    this.digDown();
+                    this.down();
+                } else {
+                    this.digUp();
+                    this.up();
+                }
+
             }
         }
     }
 
     goTo(x: number, y:number, z:number) {
+        this.log(`moving to ${x}x, ${y}x, ${z}z`);
+        this.draw();
         while(this.y != y) {
-            if(this.y < 0) {
-                this.up();
+            if(this.y < y) {
+                turtle.up();
+                this.addStep();
+                this.y++;
+            } else if(this.y > y) {
+                this.addStep();
+                turtle.down();
+                this.y++;
             } else {
-                this.down();
+                break;
             }
+            this.draw();
         }
         while(this.x != x) {
-            if(this.x < 0) {
+            if(this.x < x) {
                 this.face(Direction.east);
-                this.forward();
-            }
-            if(this.x > 0) {
+                this.addStep();
+                if(!turtle.forward()) {
+
+                }
+                turtle.forward();
+                this.x++;
+            } else if(this.x > x) {
                 this.face(Direction.west);
-                this.forward();
+                this.addStep();
+                turtle.dig();
+                turtle.forward();
+                this.x--;
+            } else {
+                break;
             }
+            this.draw();
         }
         while(this.z != z) {
-            if(this.z < 0) {
+            if(this.z < z) {
                 this.face(Direction.north);
-                this.forward();
-            }
-            if(this.z > 0) {
+                this.addStep();
+                turtle.dig();
+                turtle.forward();
+                this.z++;
+            } else if(this.z > z) {
                 this.face(Direction.south);
-                this.forward();
+                this.addStep();
+                turtle.dig();
+                turtle.forward();
+                this.z--;
+            } else {
+                break;
             }
+            this.draw();
         }
+        this.log("done moving");
+        this.draw();
     }
 
     quarry(extent: number, depth: number, height: number) {
         term.setTextColor(colors.white);
         term.setBackgroundColor(colors.blue);
         this.job = new MiningJob(extent, height, depth);
+        this.log("check whitelist.txt for targeted blocks")
+        this.log("cleaning inventory");
+        // garbage collect at start
+        this.tick();
         this.mineBox(extent, depth, height);
-        this.goTo(0,0,0)
+        this.goTo(0,0,0);
+        this.log("cleaning inventory");
+        this.draw();
+        // garbage collect at end
+        this.tick();
+        this.log("dumping inventory");
+        this.draw();
         this.face(Direction.south);
         this.dumpInventory();
         term.setTextColor(colors.green);
         this.log("Done!");
+        this.face(Direction.north);
         this.draw();
     }
+
 }
 
 function interactiveMode() {
@@ -351,14 +426,13 @@ function interactiveMode() {
     term.clear();
     term.setCursorPos(1,1);
     io.write(`<<<< QUARRY BY KAMAII >>>>\n`);
-    io.write("WARNING: Turtle will mine to the right inclusively \n(the block the turtle is in counts)\n")
+    io.write("WARNING: Turtle will mine to the right inclusively\n")
     io.write("input the amount of blocks to the right \n")
     term.setTextColor(colors.black);
     const xS = read();
     term.setBackgroundColor(colours.blue);
     term.clear();
     term.setCursorPos(1,1);
-
     term.setTextColor(colours.white);
     io.write("input the amount of blocks forward\n");
     term.setTextColor(colours.black);
@@ -366,13 +440,12 @@ function interactiveMode() {
     term.setBackgroundColor(colours.blue);
     term.clear();
     term.setCursorPos(1,1);
-
     term.setTextColor(colors.white);
     io.write("input the amount of blocks down (negative for up)\n");
     term.setTextColor(colours.black);
     const yS = read();
     term.setTextColor(colors.white);
-    const t = new MiningTurtle();
+    const t = new MiningTurtle(WHITELIST);
     const x = tonumber(xS);
     const y = tonumber(yS);
     const z = tonumber(zS);
@@ -383,17 +456,18 @@ function interactiveMode() {
     t.quarry(x, z, y);
 }
 
-function main(...args: string[]) {
-    if(args.length <= 3) {
+function main(...args) {
+    if(args.length != 3) {
         interactiveMode();
-        return;
+    } else {
+        const [ xS, yS, zS ] = args;
+        const x = tonumber(xS, 10);
+        const y = tonumber(yS, 10);
+        const z = tonumber(zS, 10);
+        const t = new MiningTurtle(WHITELIST);
+        t.quarry(x, z, y);
     }
-    const [ xS, yS, zS ] = args;
-    const x = tonumber(xS, 10);
-    const y = tonumber(yS, 10);
-    const z = tonumber(zS, 10);
-    const t = new MiningTurtle();
-    t.quarry(x, z, y);
+
 }
 
 main(...$vararg);
